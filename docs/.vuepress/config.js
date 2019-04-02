420fa5b932489c0e43a5f3b3b5de243ed05f6f6e
吴贻韡 <yiwei.wu@ipiaoniu.com>
2019-04-02 16:16:15 +0800

    feat: docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,14 @@ module.exports = {
     title: 'Vue Pdfmake',
     description: 'Vue生成pdf文件',
     base: '/vue-pdfmake/',
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            {
+                text: 'PdfMake',
+                link:
+                    'https://pdfmake.github.io/docs/getting-started/client-side/supported-browsers/',
+            },
+        ],
+    },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,14 @@
-# Hello VuePress!
+---
+home: true
+heroImage: /hero.png
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+    - title: Pdfmake
+      details: 使用pdfmake作为内部驱动
+    - title: Vue驱动
+      details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+    - title: 中文支持
+      details: 已打包中文字体，支持输出中文。
+footer: MIT Licensed | Copyright © 2019-present Mizuka Wu
+---
