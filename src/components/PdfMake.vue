e1a3f691f51df79b371669e3432ab34119b9c759
吴贻韡 <yiwei.wu@ipiaoniu.com>
2019-04-02 11:57:15 +0800

    feat: prettier

---
+++ src/components/PdfMake.vue
@@ -0,0 +1,11 @@
+<template> </template>
+
+<script>
+import pdfMake from 'pdfmake'
+import pdfFonts from 'pdfmake-chinese/vfs_fonts'
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs
+export default {}
+</script>
+
+<style></style>
