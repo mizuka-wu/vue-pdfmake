051cda8eefb07bda11520fdb26ad12a10695340a
吴贻韡 <yiwei.wu@ipiaoniu.com>
2019-04-03 16:32:16 +0800

    fix: version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    base: '/vue-pdfmake/',
-    title: 'Vue Pdfmake',
-    description: 'Vue生成pdf文件',
-    themeConfig: {
-        nav: [
-            { text: '主页', link: '/' },
-            {
-                text: 'PdfMake',
-                link:
-                    'https://pdfmake.github.io/docs/getting-started/client-side/supported-browsers/',
-            },
-        ],
-    },
+  base: '/vue-pdfmake/',
+  title: 'Vue Pdfmake',
+  description: 'Vue生成pdf文件',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      {
+        text: 'PdfMake',
+        link:
+          'https://pdfmake.github.io/docs/getting-started/client-side/supported-browsers/',
+      },
+    ],
+  },
 }
